6c0c7189cd8b9a22f7824ec5ac62f47279358d66
Max Murakami <mmurakam@redhat.com>
2022-11-30 16:11:49 +0100

    added S3 endpoint URL in elyra runtime image

--- odh-kfp-modelmesh/container-images/custom-notebook/setup-elyra.sh
+++ odh-kfp-modelmesh/container-images/custom-notebook/setup-elyra.sh
@@ -14,7 +14,7 @@ if [[ ! -f "$DEFAULT_RUNTIME_FILE" ]]; then
                                   --display_name=Default \
                                   --auth_type=NO_AUTHENTICATION \
                                   --api_endpoint=http://ds-pipeline-ui."$KF_DEPLOYMENT_NAMESPACE".svc.cluster.local:3000/pipeline \
-                                  --cos_endpoint=http://s3.openshift-storage.svc.cluster.local \
+                                  --cos_endpoint="$S3_ENDPOINT_URL" \
                                   --cos_auth_type=USER_CREDENTIALS \
                                   --cos_username="$AWS_ACCESS_KEY_ID" \
                                   --cos_password="$AWS_SECRET_ACCESS_KEY" \
