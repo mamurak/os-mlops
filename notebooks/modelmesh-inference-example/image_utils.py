33548b7c4265f9b25bb7babe87fdc2b700daabba
Max Murakami <mmurakam@redhat.com>
2023-01-05 15:57:25 +0000

    finished face detection sample notebook

--- notebooks/modelmesh-inference-example/image_utils.py
+++ notebooks/modelmesh-inference-example/image_utils.py
@@ -1,6 +1,36 @@
+import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 
-def area_of(left_top, right_bottom):
+
+def load_and_preprocess(img_path):
+    orig_image = cv2.imread(img_path)
+    orig_image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(orig_image, (640, 480))
+    image_mean = np.array([127, 127, 127])
+    image = (image - image_mean) / 128
+    image = np.transpose(image, [2, 0, 1])
+    image = np.expand_dims(image, axis=0)
+    image = image.astype(np.float32)
+    return orig_image, image
+
+
+def draw_image_and_faces(original_image, confidences, boxes):
+    boxes, labels, probs = _get_face_boxes(
+        original_image.shape[1],
+        original_image.shape[0],
+        confidences,
+        boxes,
+        prob_threshold=0.7
+    )
+    color = (255, 128, 0)
+    for i in range(boxes.shape[0]):
+        box = _scale(boxes[i, :])
+        cv2.rectangle(original_image, (box[0], box[1]), (box[2], box[3]), color, 4)
+        plt.imshow(original_image)
+
+
+def _area_of(left_top, right_bottom):
     """
     Compute the areas of rectangles given two corners.
     Args:
@@ -12,7 +42,8 @@ def area_of(left_top, right_bottom):
     hw = np.clip(right_bottom - left_top, 0.0, None)
     return hw[..., 0] * hw[..., 1]
 
-def iou_of(boxes0, boxes1, eps=1e-5):
+
+def _iou_of(boxes0, boxes1, eps=1e-5):
     """
     Return intersection-over-union (Jaccard index) of boxes.
     Args:
@@ -25,12 +56,13 @@ def iou_of(boxes0, boxes1, eps=1e-5):
     overlap_left_top = np.maximum(boxes0[..., :2], boxes1[..., :2])
     overlap_right_bottom = np.minimum(boxes0[..., 2:], boxes1[..., 2:])
 
-    overlap_area = area_of(overlap_left_top, overlap_right_bottom)
-    area0 = area_of(boxes0[..., :2], boxes0[..., 2:])
-    area1 = area_of(boxes1[..., :2], boxes1[..., 2:])
+    overlap_area = _area_of(overlap_left_top, overlap_right_bottom)
+    area0 = _area_of(boxes0[..., :2], boxes0[..., 2:])
+    area1 = _area_of(boxes1[..., :2], boxes1[..., 2:])
     return overlap_area / (area0 + area1 - overlap_area + eps)
 
-def hard_nms(box_scores, iou_threshold, top_k=-1, candidate_size=200):
+
+def _hard_nms(box_scores, iou_threshold, top_k=-1, candidate_size=200):
     """
     Perform hard non-maximum-supression to filter out boxes with iou greater
     than threshold
@@ -55,7 +87,7 @@ def hard_nms(box_scores, iou_threshold, top_k=-1, candidate_size=200):
         current_box = boxes[current, :]
         indexes = indexes[:-1]
         rest_boxes = boxes[indexes, :]
-        iou = iou_of(
+        iou = _iou_of(
             rest_boxes,
             np.expand_dims(current_box, axis=0),
         )
@@ -63,7 +95,8 @@ def hard_nms(box_scores, iou_threshold, top_k=-1, candidate_size=200):
 
     return box_scores[picked, :]
 
-def predict(width, height, confidences, boxes, prob_threshold, iou_threshold=0.5, top_k=-1):
+
+def _get_face_boxes(width, height, confidences, boxes, prob_threshold, iou_threshold=0.5, top_k=-1):
     """
     Select boxes that contain human faces
     Args:
@@ -80,25 +113,19 @@ def predict(width, height, confidences, boxes, prob_threshold, iou_threshold=0.5
     """
     boxes = boxes[0]
     confidences = confidences[0]
-    #print(boxes)
-    #print(confidences)
 
     picked_box_probs = []
     picked_labels = []
-    #print(f'confidences shape[1]: {confidences.shape[1]}')
     for class_index in range(1, confidences.shape[1]):
-        #print(confidences.shape[1])
         probs = confidences[:, class_index]
-        #print(probs)
         mask = probs > prob_threshold
         probs = probs[mask]
 
         if probs.shape[0] == 0:
             continue
         subset_boxes = boxes[mask, :]
-        #print(subset_boxes)
         box_probs = np.concatenate([subset_boxes, probs.reshape(-1, 1)], axis=1)
-        box_probs = hard_nms(box_probs,
+        box_probs = _hard_nms(box_probs,
            iou_threshold=iou_threshold,
            top_k=top_k,
            )
@@ -112,3 +139,14 @@ def predict(width, height, confidences, boxes, prob_threshold, iou_threshold=0.5
     picked_box_probs[:, 2] *= width
     picked_box_probs[:, 3] *= height
     return picked_box_probs[:, :4].astype(np.int32), np.array(picked_labels), picked_box_probs[:, 4]
+
+
+def _scale(box):
+    width = box[2] - box[0]
+    height = box[3] - box[1]
+    maximum = max(width, height)
+    dx = int((maximum - width)/2)
+    dy = int((maximum - height)/2)
+
+    bboxes = [box[0] - dx, box[1] - dy, box[2] + dx, box[3] + dy]
+    return bboxes
