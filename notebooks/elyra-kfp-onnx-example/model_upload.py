4ad3482d56ec6db42bacee09370fb631cfdb7126
Max Murakami <mmurakam@redhat.com>
2023-01-19 08:46:55 +0000

    added model to elyra kfp onnx example

--- notebooks/elyra-kfp-onnx-example/model_upload.py
+++ notebooks/elyra-kfp-onnx-example/model_upload.py
@@ -10,7 +10,16 @@ s3_secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 s3_bucket_name = os.environ.get('AWS_S3_BUCKET')
 
 
-def initialize_s3_client(s3_endpoint_url, s3_access_key, s3_secret_key):
+def upload_model(model_object_name='model.onnx'):
+    s3_client = _initialize_s3_client(
+        s3_endpoint_url=s3_endpoint_url,
+        s3_access_key=s3_access_key,
+        s3_secret_key=s3_secret_key
+    )
+    _do_upload(s3_client, model_object_name)
+
+
+def _initialize_s3_client(s3_endpoint_url, s3_access_key, s3_secret_key):
     print('initializing S3 client')
     s3_client = client(
         's3', aws_access_key_id=s3_access_key,
@@ -20,7 +29,7 @@ def initialize_s3_client(s3_endpoint_url, s3_access_key, s3_secret_key):
     return s3_client
 
 
-def upload_model(s3_client, object_name):
+def _do_upload(s3_client, object_name):
     print(f'uploading model to {object_name}')
     try:
         s3_client.upload_file('model.onnx', s3_bucket_name, object_name)
@@ -30,9 +39,4 @@ def upload_model(s3_client, object_name):
 
 
 if __name__ == '__main__':
-    s3_client = initialize_s3_client(
-        s3_endpoint_url=s3_endpoint_url,
-        s3_access_key=s3_access_key,
-        s3_secret_key=s3_secret_key
-    )
-    upload_model(s3_client, model_object_name)
+    upload_model(model_object_name)
