acba170d0bc3966cff57f0ba4ad346e0217fdabc
Max Murakami <mmurakam@redhat.com>
2023-06-12 15:53:37 +0000

    updated object detection model training after DSP release

--- notebooks/object-detection-example/model-training/model_upload.py
+++ notebooks/object-detection-example/model-training/model_upload.py
@@ -1,22 +1,31 @@
-import os
+from os import environ
+from datetime import datetime
 
 from boto3 import client
 
 
-model_object_name = os.environ.get('MODEL_OBJECT_NAME', 'model.onnx')
-s3_endpoint_url = os.environ.get('AWS_S3_ENDPOINT')
-s3_access_key = os.environ.get('AWS_ACCESS_KEY_ID')
-s3_secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-s3_bucket_name = os.environ.get('AWS_S3_BUCKET')
+model_object_prefix = environ.get('MODEL_OBJECT_PREFIX', 'model')
+s3_endpoint_url = environ.get('AWS_S3_ENDPOINT')
+s3_access_key = environ.get('AWS_ACCESS_KEY_ID')
+s3_secret_key = environ.get('AWS_SECRET_ACCESS_KEY')
+s3_bucket_name = environ.get('AWS_S3_BUCKET')
 
 
-def upload_model(model_object_name='model.onnx'):
+def upload_model(model_object_prefix='model', version=''):
     s3_client = _initialize_s3_client(
         s3_endpoint_url=s3_endpoint_url,
         s3_access_key=s3_access_key,
         s3_secret_key=s3_secret_key
     )
+    model_object_name = _generate_model_name(
+        model_object_prefix, version=version
+    )
     _do_upload(s3_client, model_object_name)
+
+    model_object_name_latest = _generate_model_name(
+        model_object_prefix, 'latest'
+    )
+    _do_upload(s3_client, model_object_name_latest)
 
 
 def _initialize_s3_client(s3_endpoint_url, s3_access_key, s3_secret_key):
@@ -27,6 +36,16 @@ def _initialize_s3_client(s3_endpoint_url, s3_access_key, s3_secret_key):
         endpoint_url=s3_endpoint_url,
     )
     return s3_client
+
+
+def _generate_model_name(model_object_prefix, version=''):
+    version = version if version else _timestamp()
+    model_name = f'{model_object_prefix}-{version}.onnx'
+    return model_name
+
+
+def _timestamp():
+    return datetime.now().strftime('%y%m%d%H%M')
 
 
 def _do_upload(s3_client, object_name):
@@ -40,4 +59,4 @@ def _do_upload(s3_client, object_name):
 
 
 if __name__ == '__main__':
-    upload_model(model_object_name)
+    upload_model(model_object_prefix)
