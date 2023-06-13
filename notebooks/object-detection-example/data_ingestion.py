8c3f4de4d46f356b11c4ddf23cc2403308ae7c10
Max Murakami <mmurakam@redhat.com>
2023-06-13 16:06:23 +0000

    updated object detection offline scoring pipeline

--- notebooks/object-detection-example/data_ingestion.py
+++ notebooks/object-detection-example/data_ingestion.py
@@ -6,10 +6,10 @@ from boto3 import client
 def ingest_data(bucket_name='', data_folder='./data'):
     print('Commencing data ingestion.')
 
-    s3_endpoint_url = environ.get('S3_ENDPOINT_URL')
-    s3_access_key = environ.get('S3_ACCESS_KEY')
-    s3_secret_key = environ.get('S3_SECRET_KEY')
-    s3_bucket_name = bucket_name or environ.get('S3_DATA_BUCKET_NAME')
+    s3_endpoint_url = environ.get('AWS_S3_ENDPOINT')
+    s3_access_key = environ.get('AWS_ACCESS_KEY_ID')
+    s3_secret_key = environ.get('AWS_SECRET_ACCESS_KEY')
+    s3_bucket_name = environ.get('AWS_S3_BUCKET')
 
     print(f'Downloading data from bucket "{s3_bucket_name}" '
           f'from S3 storage at {s3_endpoint_url}')
--- notebooks/object-detection-example/model_loading.py
+++ notebooks/object-detection-example/model_loading.py
@@ -3,16 +3,17 @@ from os import environ
 from boto3 import client
 
 
-def load_model(bucket_name='', model_file_name=''):
+def load_model(bucket_name='', model_object_name=''):
     print('Commencing model loading.')
 
-    s3_endpoint_url = environ.get('S3_ENDPOINT_URL')
-    s3_access_key = environ.get('S3_ACCESS_KEY')
-    s3_secret_key = environ.get('S3_SECRET_KEY')
-    s3_bucket_name = bucket_name or environ.get('S3_MODEL_BUCKET_NAME')
-    model_file_name = model_file_name or environ.get('MODEL_FILE_NAME')
+    s3_endpoint_url = environ.get('AWS_S3_ENDPOINT')
+    s3_access_key = environ.get('AWS_ACCESS_KEY_ID')
+    s3_secret_key = environ.get('AWS_SECRET_ACCESS_KEY')
+    s3_bucket_name = environ.get('AWS_S3_BUCKET')
+    model_object_name = model_object_name or environ.get('model_object_name')
 
-    print(f'Downloading model from bucket {s3_bucket_name} '
+    print(f'Downloading model {model_object_name} '
+          f'from bucket {s3_bucket_name} '
           f'from S3 storage at {s3_endpoint_url}')
 
     s3_client = client(
@@ -21,7 +22,7 @@ def load_model(bucket_name='', model_file_name=''):
     )
 
     s3_client.download_file(
-        s3_bucket_name, model_file_name, 'model.onnx'
+        s3_bucket_name, model_object_name, 'model.onnx'
     )
 
     print('Finished model loading.')
