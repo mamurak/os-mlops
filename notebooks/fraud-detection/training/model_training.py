b699aca5d18097c1289dbec50ab8e0b8ce77739c
Max Murakami <mmurakam@redhat.com>
2023-06-15 13:27:16 +0000

    added fraud detection training

---
+++ notebooks/fraud-detection/training/model_training.py
@@ -0,0 +1,66 @@
+from os import environ
+
+from joblib import dump, load
+from pandas import read_parquet
+from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+
+
+feature_columns = [
+    'user_id', 'amount', 'trans_type', 'foreign', 'interarrival'
+]
+max_regression_iterations = int(environ.get('max_regression_iterations', 500))
+
+
+def train_model_pipeline(data_folder='./data'):
+    print('training model')
+
+    df = read_parquet(f'{data_folder}/training-data.parquet')
+    train, test = train_test_split(df, random_state=43)
+
+    feature_pipeline = _load_feature_pipeline()
+    model = _train_model(train, feature_pipeline)
+
+    predictions = model.predict(
+        feature_pipeline.fit_transform(test[feature_columns])
+    )
+    print(f'test results for '
+          f'max_regression_iterations={max_regression_iterations}')
+    print(classification_report(test.label.values, predictions))
+
+    pipeline = Pipeline([
+        ('features', feature_pipeline),
+        ('model', model)
+    ])
+
+    pipeline.fit(train[feature_columns], y=train['label'])
+    dump(pipeline, open('model.joblib', 'wb'))
+
+    print('model training done')
+
+
+def _load_feature_pipeline():
+    with open('feature_pipeline.joblib', 'rb') as inputfile:
+        feature_pipeline = load(inputfile)
+    return feature_pipeline
+
+
+def _train_model(train, feature_pipeline):
+    fraud_frequency = (
+        train[train["label"] == "fraud"]["timestamp"].count() /
+        train["timestamp"].count()
+    )
+    train.loc[train["label"].eq("legitimate"), "weights"] = fraud_frequency
+    train.loc[train["label"].eq("fraud"), "weights"] = (1 - fraud_frequency)
+
+    model = LogisticRegression(max_iter=max_regression_iterations)
+
+    svecs = feature_pipeline.fit_transform(train[feature_columns])
+    model.fit(svecs, train["label"], sample_weight=train["weights"])
+    return model
+
+
+if __name__ == '__main__':
+    train_model_pipeline(data_folder='/data')
---
+++ notebooks/fraud-detection/training/preprocessing.py
@@ -0,0 +1,45 @@
+from joblib import dump
+from pandas import read_parquet
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import RobustScaler, OneHotEncoder
+
+
+def preprocess_data(data_folder='./data'):
+    print('preprocessing data')
+
+    df = read_parquet(f'{data_folder}/training-data.parquet')
+    train, _ = train_test_split(df, random_state=43)
+
+    tt_xform = (
+        'onehot',
+        OneHotEncoder(
+            handle_unknown='ignore',
+            categories=[[
+                'online', 'contactless', 'chip_and_pin', 'manual', 'swipe'
+            ]]
+        ),
+        ['trans_type']
+    )
+
+    impute_and_scale = Pipeline([
+        ('median_imputer', SimpleImputer(strategy='median')),
+        ('interarrival_scaler', RobustScaler())
+    ])
+    ia_scaler = ('interarrival_scaler', impute_and_scale, ['interarrival'])
+    amount_scaler = ('amount_scaler', RobustScaler(), ['amount'])
+
+    all_xforms = ColumnTransformer(transformers=([ia_scaler, amount_scaler, tt_xform]))
+    feature_pipeline = Pipeline([('feature_extraction', all_xforms)])
+
+    feature_columns = ['user_id', 'amount', 'trans_type', 'foreign', 'interarrival']
+    feature_pipeline.fit(train[feature_columns])
+    dump(feature_pipeline, open('feature_pipeline.joblib', 'wb'))
+
+    print('data processing done')
+
+
+if __name__ == '__main__':
+    preprocess_data(data_folder='/data')
