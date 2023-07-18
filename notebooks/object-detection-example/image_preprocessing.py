410c02c55e2d809dca013238e00eae68e7bd0427
Max Murakami <mmurakam@redhat.com>
2023-07-18 11:40:45 +0000

    implemented yolov5 inference

--- notebooks/object-detection-example/image_preprocessing.py
+++ notebooks/object-detection-example/image_preprocessing.py
@@ -1,27 +1,50 @@
+import cv2
 import numpy as np
-from PIL import Image
 
 
-def preprocess(image_path):
-    image = Image.open(image_path)
-    model_image_size = (640, 640)
-    boxed_image = _letterbox_image(image, tuple(reversed(model_image_size)))
-    image_data = np.array(boxed_image, dtype='float32')
-    image_data /= 255.
-    image_data = np.transpose(image_data, [2, 0, 1])
-    image_data = np.expand_dims(image_data, 0)
-    return image_data
+def preprocess(image_path, image_size=640):
+    image = cv2.imread(image_path)
+    image, ratio, dwdh = _letterbox_image(image, image_size, auto=False)
+    image = image.transpose((2, 0, 1))  # HWC->CHW for PyTorch model
+    image = np.expand_dims(image, 0)  # Model expects an array of images
+    image = np.ascontiguousarray(image)
+    # Speed up things by rewriting the array contiguously in memory
+
+    im = image.astype(np.float32)  # Model expects float32 data type
+    im /= 255  # Convert RGB values [0-255] to [0-1]
+    return im, ratio, dwdh
 
 
-def _letterbox_image(image, size):
-    '''resize image with unchanged aspect ratio using padding'''
-    iw, ih = image.size
-    w, h = size
-    scale = min(w/iw, h/ih)
-    nw = int(iw*scale)
-    nh = int(ih*scale)
+def _letterbox_image(
+        im, image_size, color=(114, 114, 114), auto=True, scaleup=True, stride=32):
 
-    image = image.resize((nw, nh), Image.BICUBIC)
-    new_image = Image.new('RGB', size, (128, 128, 128))
-    new_image.paste(image, ((w-nw)//2, (h-nh)//2))
-    return new_image
+    # Resize and pad image while meeting stride-multiple constraints
+    shape = im.shape[:2]  # current shape [height, width]
+    new_shape = image_size
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better val mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    im = cv2.copyMakeBorder(
+        im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color
+    )  # add border
+
+    return im, r, (dw, dh)
--- notebooks/object-detection-example/object_rendering.py
+++ notebooks/object-detection-example/object_rendering.py
@@ -1,57 +1,41 @@
+import random
+
+import cv2
+from matplotlib import pyplot as plt
 import numpy as np
-from PIL import Image, ImageColor, ImageDraw, ImageFont
+
+from classes import classes
 
 
-def draw_boxes(image, boxes, scores, classes):
-    """Overlay labeled boxes on an image with formatted scores and label names."""
-    colors = list(ImageColor.colormap.values())
-    class_colors = {}
-    font = ImageFont.load_default()
-    image_pil = Image.open(image)
-
-    for index, class_ in enumerate(classes):
-        box = boxes[index]
-        display_str = f'{class_}: {int(100 * scores[index])}%'
-        if class_ not in class_colors:
-            class_colors[class_] = colors[hash(class_) * 8 % len(colors)]
-        color = class_colors.get(class_)
-        _draw_bounding_box_on_image(
-            image_pil, box[0], box[1], box[2], box[3], color, font,
-            display_str_list=[display_str]
+def draw_boxes(image_path, model_output, ratio, dwdh):
+    image = cv2.imread(image_path)  # Read image
+    colors = {
+        name: [
+            random.randint(0, 255) for _ in range(3)
+        ] for i, name in enumerate(classes)
+    }
+    for i, (x0, y0, x1, y1, score, cls_id) in enumerate(model_output):
+        box = np.array([x0, y0, x1, y1])
+        box -= np.array(dwdh*2)
+        box /= ratio
+        box = box.round().astype(np.int32).tolist()
+        cls_id = int(cls_id)
+        score = round(float(score), 3)
+        name = classes[cls_id]
+        color = colors[name]
+        name += ' '+str(score)
+        cv2.rectangle(image, box[:2], box[2:], color,2)
+        cv2.putText(
+            image,
+            name,
+            (box[0], box[1] - 2),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.75,
+            [0, 255, 0],
+            thickness=2
         )
-    return image_pil
-    image_pil.show()
-
-
-def _draw_bounding_box_on_image(
-        image, ymin, xmin, ymax, xmax, color, font,
-        thickness=4, display_str_list=()):
-    """Adds a bounding box to an image."""
-    draw = ImageDraw.Draw(image)
-    im_width, im_height = image.size
-    width_scaling_factor = im_width / 416
-    height_scaling_factor = im_height / 416
-    (left, right, top, bottom) = (
-        xmin * width_scaling_factor,
-        xmax * width_scaling_factor,
-        ymin * height_scaling_factor,
-        ymax * height_scaling_factor,
-    )
-    draw.line([(left, top), (left, bottom), (right, bottom), (right, top),
-               (left, top)], width=thickness, fill=color)
-
-    display_str_heights = [font.getsize(ds)[1] for ds in display_str_list]
-    total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
-    if top > total_display_str_height:
-        text_bottom = top
-    else:
-        text_bottom = top + total_display_str_height
-
-    for display_str in display_str_list[::-1]:
-        text_width, text_height = font.getsize(display_str)
-        margin = np.ceil(0.05 * text_height)
-        draw.rectangle([(left, text_bottom - text_height - 2 * margin),
-                        (left + text_width, text_bottom)], fill=color)
-        draw.text((left + margin, text_bottom - text_height - margin),
-                  display_str, fill="black", font=font)
-        text_bottom -= text_height - 2 * margin
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    fig = plt.gcf()
+    fig.set_size_inches(24, 12)
+    plt.axis('off')
+    plt.imshow(img)
