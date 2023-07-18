3838dc279eaba134fe177e5400e14949b02609b9
Max Murakami <mmurakam@redhat.com>
2023-07-18 11:40:45 +0000

    implemented yolov5 inference

--- notebooks/object-detection-example/object_detection.py
+++ notebooks/object-detection-example/object_detection.py
@@ -1,14 +1,24 @@
 import numpy as np
 from requests import post
+import torch
+import torchvision
 
 from classes import classes
 
 
-def detect_objects(image, prediction_url, token):
+def detect_objects(
+        image,
+        prediction_url,
+        token,
+        confidence_threshold=0.2,
+        iou_threshold=0.6):
+
     payload = _serialize(image)
     model_response = _get_model_response(payload, prediction_url, token)
-    boxes, scores, class_labels = _postprocess(model_response)
-    return boxes, scores, class_labels
+    processed_output = _postprocess(
+        model_response, confidence_threshold, iou_threshold
+    )
+    return processed_output
 
 
 def _serialize(image):
@@ -19,13 +29,7 @@ def _serialize(image):
                 'shape': [1, 3, 640, 640],
                 'datatype': 'FP32',
                 'data': image.flatten().tolist(),
-            },
-            #{
-            #    'name': 'image_shape',
-            #    'shape': [1, 2],
-            #    'datatype': 'FP32',
-            #    'data': [416, 416],
-            #},
+            }
         ]
     }
     return payload
@@ -45,21 +49,176 @@ def _get_model_response(payload, prediction_url, token):
     except:
         print(f'Failed to extract model output from service response.\n'
               f'Service response: {response}')
-    unpacked_output = [_unpack(item) for item in model_output]
+    unpacked_output = _unpack(model_output)
     return unpacked_output
 
 
-def _unpack(response_item):
-    return np.array(response_item['data']).reshape(response_item['shape'])
+def _unpack(model_output):
+    arr = np.array(model_output[0]['data'])
+    # Get the response data as a NumPy Array
+
+    output = torch.tensor(arr)  # Create a tensor from array
+    prediction_columns_number = 5 + len(classes)
+    # Model returns model returns [xywh, conf, class0, class1, ...]
+
+    output = output.reshape(
+        1,
+        int(int(output.shape[0])/prediction_columns_number),
+        prediction_columns_number
+    )  # Reshape the flat array prediction
+
+    return output
 
 
-def _postprocess(model_response):
-    boxes, scores, detected_classes = [], [], []
+def _postprocess(
+        prediction,
+        conf_thres,
+        iou_thres,
+        classes=None,
+        agnostic=False,
+        multi_label=False,
+        labels=(),
+        max_det=300,
+        nm=0,  # number of masks
+):
+    """Non-Maximum Suppression (NMS) on inference results to reject 
+    overlapping detections
 
-    for item in model_response[0]:
-        boxes.append(item[:3])
-        item_scores = item[5:8]
-        detected_classes.append(np.argmax(item_scores))
-        scores.append(np.max(item_scores))
+    Returns:
+        list of detections, on (n,6) tensor per image [xyxy, conf, cls]
+    """
 
-    return boxes, scores, detected_classes
+    if isinstance(prediction, (list, tuple)):
+        # YOLOv5 model in validation model, output = (inference_out, loss_out)
+        prediction = prediction[0]  # select only inference output
+
+    device = prediction.device
+    mps = 'mps' in device.type  # Apple MPS
+    if mps:  # MPS not fully supported yet, convert tensors to CPU before NMS
+        prediction = prediction.cpu()
+    bs = prediction.shape[0]  # batch size
+    nc = prediction.shape[2] - nm - 5  # number of classes
+    xc = prediction[..., 4] > conf_thres  # candidates
+
+    # Settings
+    max_wh = 7680  # (pixels) maximum box width and height
+    max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
+    redundant = True  # require redundant detections
+    multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
+    merge = False  # use merge-NMS
+
+    mi = 5 + nc  # mask start index
+    output = [torch.zeros((0, 6 + nm), device=prediction.device)] * bs
+    for xi, x in enumerate(prediction):  # image index, image inference
+        # Apply constraints
+        # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0
+        # width-height
+        x = x[xc[xi]]  # confidence
+
+        # Cat apriori labels if autolabelling
+        if labels and len(labels[xi]):
+            lb = labels[xi]
+            v = torch.zeros((len(lb), nc + nm + 5), device=x.device)
+            v[:, :4] = lb[:, 1:5]  # box
+            v[:, 4] = 1.0  # conf
+            v[range(len(lb)), lb[:, 0].long() + 5] = 1.0  # cls
+            x = torch.cat((x, v), 0)
+
+        # If none remain process next image
+        if not x.shape[0]:
+            continue
+
+        # Compute conf
+        x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
+
+        # Box/Mask
+        box = xywh2xyxy(x[:, :4])
+        # center_x, center_y, width, height) to (x1, y1, x2, y2)
+
+        mask = x[:, mi:]  # zero columns if no masks
+
+        # Detections matrix nx6 (xyxy, conf, cls)
+        if multi_label:
+            i, j = (x[:, 5:mi] > conf_thres).nonzero(as_tuple=False).T
+            x = torch.cat(
+                (box[i], x[i, 5 + j, None], j[:, None].float(), mask[i]), 1
+            )
+        else:  # best class only
+            conf, j = x[:, 5:mi].max(1, keepdim=True)
+            x = torch.cat((box, conf, j.float(), mask), 1)[
+                conf.view(-1) > conf_thres
+            ]
+
+        # Filter by class
+        if classes is not None:
+            x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
+
+        # Check shape
+        n = x.shape[0]  # number of boxes
+        if not n:  # no boxes
+            continue
+        elif n > max_nms:  # excess boxes
+            x = x[x[:, 4].argsort(descending=True)[:max_nms]]  # sort by confidence
+        else:
+            x = x[x[:, 4].argsort(descending=True)]  # sort by confidence
+
+        # Batched NMS
+        c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
+        boxes, scores = x[:, :4] + c, x[:, 4]
+        # boxes (offset by class), scores
+
+        i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
+        if i.shape[0] > max_det:  # limit detections
+            i = i[:max_det]
+
+        if merge and (1 < n < 3E3):
+            # Merge NMS (boxes merged using weighted mean)
+            # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
+            iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
+            weights = iou * scores[None]  # box weights
+            x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(
+                1, keepdim=True
+            )  # merged boxes
+            if redundant:
+                i = i[iou.sum(1) > 1]  # require redundancy
+
+        output[xi] = x[i]
+        if mps:
+            output[xi] = output[xi].to(device)
+
+    return output[0]
+
+
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2]
+    # where xy1=top-left, xy2=bottom-right
+
+    y = torch.zeros_like(x) if isinstance(x, torch.Tensor) else np.zeros_like(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
+
+
+def box_iou(box1, box2, eps=1e-7):
+    # https://github.com/pytorch/vision/blob/master/torchvision/ops/boxes.py
+    """
+    Return intersection-over-union (Jaccard index) of boxes.
+    Both sets of boxes are expected to be in (x1, y1, x2, y2) format.
+    Arguments:
+        box1 (Tensor[N, 4])
+        box2 (Tensor[M, 4])
+    Returns:
+        iou (Tensor[N, M]): the NxM matrix containing the pairwise
+            IoU values for every element in boxes1 and boxes2
+    """
+
+    # inter(N,M) = (rb(N,M,2) - lt(N,M,2)).clamp(0).prod(2)
+    (a1, a2), (b1, b2) = box1.unsqueeze(1).chunk(2, 2), box2.unsqueeze(0).chunk(
+        2, 2
+    )
+    inter = (torch.min(a2, b2) - torch.max(a1, b1)).clamp(0).prod(2)
+
+    # IoU = inter / (area1 + area2 - inter)
+    return inter / ((a2 - a1).prod(2) + (b2 - b1).prod(2) - inter + eps)
