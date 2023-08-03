0574bc7da5585cfc444f79f4d780da2a21993dd8
Max Murakami <mmurakam@redhat.com>
2023-08-03 08:49:00 +0000

    minor changes in fraud detection onnx example

--- notebooks/fraud-detection-onnx/offline-scoring/scoring.py
+++ notebooks/fraud-detection-onnx/offline-scoring/scoring.py
@@ -15,7 +15,7 @@ def predict(data_folder='./data'):
     class_map_array = array(['no fraud', 'fraud'])
     mapped_results = class_map_array[results]
 
-    print(f'Prediction results: {mapped_results}')
+    print(f'Scored data set. Writing report.')
 
     column_names = [f'V{i}' for i in range(1, 31)]
     report = DataFrame(X, columns=column_names)
@@ -23,7 +23,7 @@ def predict(data_folder='./data'):
 
     report.to_csv(f'{data_folder}/predictions.csv')
 
-    print('Offline scoring complete.')
+    print('Wrote report. Offline scoring complete.')
 
 
 if __name__ == '__main__':
