a0348c221544ec6ffe3c55d23d3500086b2435df
Max Murakami <mmurakam@redhat.com>
2024-02-27 23:10:26 +0000

    updated codeflare notebooks

--- notebooks/codeflare-examples/guided-demos/mnist.py
+++ notebooks/codeflare-examples/guided-demos/mnist.py
@@ -18,7 +18,6 @@ import os
 import torch
 from pytorch_lightning import LightningModule, Trainer
 from pytorch_lightning.callbacks.progress import TQDMProgressBar
-from pytorch_lightning.loggers import CSVLogger
 from torch import nn
 from torch.nn import functional as F
 from torch.utils.data import DataLoader, random_split
@@ -119,8 +118,12 @@ class LitMNIST(LightningModule):
     def setup(self, stage=None):
         # Assign train/val datasets for use in dataloaders
         if stage == "fit" or stage is None:
-            mnist_full = MNIST(self.data_dir, train=True, transform=self.transform)
-            self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
+            mnist_full = MNIST(
+                self.data_dir, train=True, transform=self.transform
+            )
+            self.mnist_train, self.mnist_val = random_split(
+                mnist_full, [55000, 5000]
+            )
 
         # Assign test dataset for use in dataloader(s)
         if stage == "test" or stage is None:
