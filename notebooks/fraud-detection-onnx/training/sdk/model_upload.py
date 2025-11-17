fa0c3801ec267f9f57d46ffdb6cc35ef67a0334a
Max Murakami <mmurakam@redhat.com>
2025-11-17 15:08:29 +0000

    updated fraud detection kfp pipeline
    model registration for kserve compatible storage layout

--- notebooks/fraud-detection-onnx/training/sdk/model_upload.py
+++ notebooks/fraud-detection-onnx/training/sdk/model_upload.py
@@ -14,13 +14,32 @@ def upload_model(
     from shutil import copy
     from string import ascii_lowercase, digits
 
+    from boto3 import client
     from model_registry import ModelRegistry
-    from model_registry.utils import S3Params
+    from model_registry.utils import s3_uri_from
+
+    def _initialize_s3_client(s3_endpoint_url, s3_access_key, s3_secret_key):
+        print('initializing S3 client')
+        s3_client = client(
+            's3', aws_access_key_id=s3_access_key,
+            aws_secret_access_key=s3_secret_key,
+            endpoint_url=s3_endpoint_url,
+        )
+        return s3_client
 
     def _generate_version_id():
         alphabet = ascii_lowercase + digits
         version_id = ''.join(choices(alphabet, k=8))
         return version_id
+
+    def _do_upload(s3_client, object_name):
+        print(f'uploading model to {object_name}')
+        try:
+            s3_client.upload_file('model.onnx', s3_bucket_name, object_name)
+        except Exception:
+            print(f'S3 upload to bucket {s3_bucket_name} at {s3_endpoint_url} failed!')
+            raise
+        print(f'model uploaded and available as "{object_name}"')
 
     def _register_model_version(
             model_prefix, version, model_registry_endpoint_url):
@@ -37,23 +56,15 @@ def upload_model(
         probabilities for non-fraud / fraud. See sample:
         https://github.com/mamurak/os-mlops/blob/main/notebooks/fraud-detection-onnx/online-scoring.ipynb
         '''
-        s3_params = S3Params(
-            bucket_name=s3_bucket_name,
-            s3_prefix=model_prefix,
-            access_key_id=s3_access_key,
-            secret_access_key=s3_secret_key,
-            endpoint_url=s3_endpoint_url
-        )
-        copy(model.path, 'model.onnx')
-        registry.upload_artifact_and_register_model(
-            name='fraud-detection',
-            model_files_path='model.onnx',
-            upload_params=s3_params,
+
+        registry.register_model(
+            'fraud-detection',
+            uri=s3_uri_from(model_prefix, s3_bucket_name),
             version=version,
             description=model_description,
             model_format_name='onnx',
             model_format_version='1',
-            storage_key=s3_secret_name,
+            storage_key='aws-connection-fraud-detection',
             metadata={
                 'accuracy': str(training_metrics['accuracy'][-1]),
                 'epoch_count': str(training_metrics['epoch_count']),
@@ -62,7 +73,7 @@ def upload_model(
                 'onnx': '',
             }
         )
-        print('model registration complete.')
+        print(f'model uploaded to {model_prefix} and registered as version {version}')
 
     def _instantiate_model_registry(model_registry_endpoint_url):
         sa_token_file_path = '/var/run/secrets/kubernetes.io/serviceaccount/token'
@@ -88,8 +99,18 @@ def upload_model(
     s3_secret_name = 'aws-connection-fraud-detection'
     model_registry_endpoint_url = environ.get('MODEL_REGISTRY_ENDPOINT_URL')
 
+    s3_client = _initialize_s3_client(
+        s3_endpoint_url=s3_endpoint_url,
+        s3_access_key=s3_access_key,
+        s3_secret_key=s3_secret_key
+    )
+
     model_version = _generate_version_id()
     model_prefix = f'models/{model_version}'
+    model_object_name = f'{model_prefix}/1/model.onnx'
+
+    copy(model.path, 'model.onnx')
+    _do_upload(s3_client, model_object_name)
 
     if model_registry_endpoint_url:
         _register_model_version(
